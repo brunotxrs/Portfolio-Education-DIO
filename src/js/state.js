4389086186d5236602597c1a30e328c98accebe4
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-06-30 11:51:01 -0400

    feat: create the module state.js for used the points of project in html for manipulation

---
+++ src/js/state.js
@@ -0,0 +1,12 @@
+
+export const state = {
+    name: document.getElementById("name"),
+    occupation: document.getElementById("occupation"),
+    photo: document.getElementById("image-profile"),
+    location: document.getElementById("location"),
+    phone: document.getElementById("phone"),
+    email:"",
+    skills:"",
+    languages:"",
+    portfolio:"",
+}

256e24d148bd96c77474661218e2424393263632
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-06-30 20:16:16 -0400

    feat: update name object of phone for contact and add new objects

--- src/js/state.js
+++ src/js/state.js
@@ -1,12 +1,19 @@
-
 export const state = {
     name: document.getElementById("name"),
     occupation: document.getElementById("occupation"),
     photo: document.getElementById("image-profile"),
     location: document.getElementById("location"),
-    phone: document.getElementById("phone"),
-    email:"",
-    skills:"",
+    contact: document.getElementById("phone"),
+    email: document.getElementById("email"),
+    skills: {
+        hardSkills: document.getElementById("hardSkills"),     
+    },
     languages:"",
     portfolio:"",
+    buttons: {
+        btnDown: document.getElementById("arrow-down"),
+        btnUp: document.getElementById("arrow-up"),
+    },
+    containerSkill: document.querySelectorAll(".selection"),
+    boxSkills: document.getElementById("box-skills"),
 }

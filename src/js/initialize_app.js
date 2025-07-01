bdfa0ef887c98b28f5fa0e379657757809996baa
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-06-30 20:19:09 -0400

    feat: import the modules description_details.js, hardSkills  and msg_load_details.js

--- src/js/initialize_app.js
+++ src/js/initialize_app.js
@@ -1,5 +1,8 @@
 import { userProfilePromise } from './profile_class.js';
-import {  state  } from './state.js'
+import {  descriptionDetails  } from './description_details.js';
+import {  hardSkills  } from './skills.js'
+import {  msgLoadDetails  } from './msg_load_details.js';
+
 
 //função assíncrona para usar o userProfilePromise
 export async function initializeApp() {
@@ -7,27 +10,15 @@ export async function initializeApp() {
         const userProfile = await userProfilePromise; // <--  INSTÂNCIA REAL
 
         if (userProfile) { // Verifica se a Promise foi resolvida com sucesso
-            state.photo.innerHTML = ''
-            const newImg = document.createElement('img');
-            newImg.src = `${userProfile.photo}`;
-            newImg.alt = `${userProfile.name}`;
-            state.photo.appendChild(newImg);
-
-            state.name.innerHTML = `${userProfile.name}`;
-            state.occupation.innerHTML = `${userProfile.occupation}`;
-            state.location.innerHTML = `${userProfile.location}`;
-
-
-            console.log("Instância do Perfil Criada e Pronta:", userProfile);
-            console.log("Habilidades:", userProfile.skills.hardSkills);
+            // chamada da função que estrutura os dados da api ao HTML
+            descriptionDetails(userProfile)
+            
+            hardSkills(userProfile)
+            
             
         } else {
-            state.photo.innerHTML = ''
-            state.photo.innerHTML = ("Loading Photo...").toLocaleUpperCase();
-            state.name.innerHTML = ("Loading Name...").toLocaleUpperCase();
-            state.occupation.innerHTML = ("Loading Occupation..").toLocaleUpperCase();
-            state.location.innerHTML = ("Loading location...").toLocaleUpperCase();
-            console.log("Não foi possível carregar o perfil.").toLocaleUpperCase();
+            // chamada da função da estrutura HTML ainda nao carregado com dados da API
+            msgLoadDetails()
         }
     } catch (error) {
         console.error("Erro na inicialização da aplicação:", error);
